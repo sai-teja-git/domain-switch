bd03bc12be46270ec10c8e1a5453101bdefd083e
sai-teja-git <saitejaspl1223@gmail.com>
2024-08-01 13:10:49 +0530

    reverted

--- src/app/view-page/view-page.component.ts
+++ src/app/view-page/view-page.component.ts
@@ -37,8 +37,7 @@ export class ViewPageComponent {
   pushData() {
     sessionStorage.setItem(this.key, this.value)
     localStorage.setItem(this.key, this.value)
-    // this.cookieService.set(this.key, this.value, (15 * 60 * 1000))
-    this.cookieService.set(this.key, this.value, (15 * 60 * 1000), "/", "onrender.com")
+    this.cookieService.set(this.key, this.value, (15 * 60 * 1000))
     // this.cookieService.set(this.key, this.value, (15 * 60 * 1000), "/", "domain-switch-base.onrender.com")
     // this.cookieService.set(this.key, this.value, (15 * 60 * 1000), "/", "domain-switch-app.onrender.com")
     this.clear()
